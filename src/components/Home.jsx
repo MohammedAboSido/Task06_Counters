ed59e586cef59d1afe70c0d40d7d4f5c3b4f3027
Mohammed A. Abo Sido <abosido2@gmail.com>
2023-03-07 14:23:38 +0200

    First Commit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,55 +4,6 @@ class Home extends React.Component {
   render() {
     return (
       <div>
-        {/* <Navbar />
-        <Button title="Read more" />
-        <SectionHeading
-          title="About Us"
-          description="Discover The Taste Of Worldclass Vegan Dishes From The Kitchen Of Go Green Dine"
-        />
-        <MenuItem
-          img={food}
-          title="Lorem ipsum dolor sit amet"
-          description="Lorem ipsum dolor sit amet"
-          code="Rs 222"
-        />
-        
-        <ReviewCard
-          img={person}
-          name="Alex andrina"
-          description="Lorem Ipsum Dolor Sit Amet, Consectetur Adipiscing Elit. Sollicitudin Hendrerit A Amet Viverra. Nunc Pretium In Amet At Mattis Cras. Quisque Commodo Mattis Ornare A Nec Odio. Pulvinar Orci, Egestas Eget Nulla Duis At Iaculis Nunc, Elit."
-        />
-        <br />
-        <BlogCard
-          img={img3}
-          body="Lorem Ipsum Dolor Sit Amet, Consectetur Adipiscing Elit. Sollicitudin"
-          title="Cooking Dining Experience"
-        />
-        <br />
-        <br />
-        <br />
-
-        <FooterInfo img={email} title="Gogreendineservice@Gmail.Com" />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <Container>
-          <FoodSection
-            title="About Us"
-            description="Discover The Taste Of Worldclass Vegan Dishes From The Kitchen Of Go Green Dine"
-            body="Lorem Ipsum Dolor Sit Amet, Consectetur Adipiscing Elit. Sollicitudin Hendrerit A Amet Viverra. Nunc Pretium In Amet At Mattis Cras. Quisque Commodo Mattis Ornare A Nec Odio. Pulvinar Orci, Egestas Eget Nulla Duis At Iaculis Nunc, Elit. Nibh Auctor Purus Nunc Tellus Pellentesque Nibh Mattis. Malesuada Integer Consectetur. Imperdiet Aliquam Quam Mauris Semper Suscipit. Molestie Maecenas Interdum Pharetra Id Velit Sed Nec.Tetur Sit Sagittis Pretium Eget Vitae Semper Pellentesque Pellentesque."
-            btnName="Read More"
-            img={img4}
-          />
-        </Container>
-        <br />
-        <br />
-        <br /> */}
         <HomePage />
       </div>
     );
